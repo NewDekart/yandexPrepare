1925ac2210b5478e13b4c2c9418a91f884a62bca
Amal Dusalin <adusalin@fil-it.ru>
2021-12-06 21:20:08 +0300

    rightWayToList

--- 23.MergekSortedLists.js
+++ 23.MergekSortedLists.js
@@ -73,3 +73,74 @@
     return answer
 
 };
+
+
+// right way to solve
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+
+
+ function mergeTwoLists (a, b) {
+
+    let node = new ListNode(0)
+
+    const result = node
+
+    while (a !== null && b !== null) {
+
+        if (a.val < b.val) {
+
+            node.next = a
+
+            a = a.next
+
+        } else {
+
+            node.next = b
+
+            b = b.next
+        }
+
+        node = node.next
+
+    }
+
+    node.next = a || b
+
+    return result.next
+
+}
+
+var mergeKLists = function(lists) {
+
+    if (lists.length === 0) {
+
+        return null
+
+    }
+
+
+    while(lists.length > 1) {
+
+        const a = lists.shift()
+
+        const b = lists.shift()
+
+        const mergedAB = mergeTwoLists(a, b)
+
+        lists.push(mergedAB)
+    }
+
+    return lists.pop()
+
+};
